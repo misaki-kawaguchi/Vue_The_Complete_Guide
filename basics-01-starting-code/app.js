f17d8b1ed36198a9cbb49177fb1a498f4eab4795
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-04 13:08:06 +0900

    add: methods

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -6,6 +6,16 @@ const app = Vue.createApp({
       vueLink: 'https://jp.vuejs.org/'
     };
   },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return 'Learn Vue!';
+      } else {
+        return 'Master Vue!';
+      }
+    }
+  }
 });
 
 app.mount('#user-goal');
