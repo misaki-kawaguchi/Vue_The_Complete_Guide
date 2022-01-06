355dd61d5fc5090f6ded54f6361e3ec0b9792e69
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-06 19:36:23 +0900

    add: game over screen

--- section04/prj-monster-01-starting-setup/app.js
+++ section04/prj-monster-01-starting-setup/app.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
-      currentRound: 0
+      currentRound: 0,
+      winner: null
     }
   },
   computed: {
@@ -19,6 +20,26 @@ const app = Vue.createApp({
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
+    }
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // ドロー
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        // プレイヤーの負け
+        this.winner = 'monster';
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // ドロー
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        // モンスターの負け
+        this.winner = 'player';
+      }
     }
   },
   methods: {
