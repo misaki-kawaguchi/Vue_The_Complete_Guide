c10ebf2e3dfc4d2a6ef07887e38ed391dee822be
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-07 00:53:29 +0900

    add: lifecycle

--- section05/behind-scenes-01-starting-setup/app.js
+++ section05/behind-scenes-01-starting-setup/app.js
@@ -16,9 +16,34 @@ const app = Vue.createApp({
       console.dir(this.$refs.userText);
     },
   },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  },
 });
 
 app.mount('#app');
+app.unmount();
 
 const app2 = Vue.createApp({
   template: `
