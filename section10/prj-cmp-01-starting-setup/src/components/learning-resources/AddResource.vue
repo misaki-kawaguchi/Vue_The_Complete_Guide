4053a585c0dc8ce6ce3f89118d7bfefedc2039af
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-18 01:17:57 +0900

    モーダル追加

--- section10/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
+++ section10/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid.</p>
+      <p>Please check all inputs and make sure you enter at least a few characters into each input field.</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -23,6 +32,11 @@
 <script>
 export default {
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
@@ -30,11 +44,15 @@ export default {
       const enteredUrl = this.$refs.linkInput.value;
 
       if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredUrl.trim() === '') {
+        this.inputIsInvalid = true;
         return;
       }
 
       this.addResource(enteredTitle, enteredDescription, enteredUrl);
     },
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
   },
 };
 </script>
